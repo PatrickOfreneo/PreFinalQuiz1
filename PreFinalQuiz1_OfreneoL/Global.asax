fc8f8ba50b9ca769a51079a032e7e1769c7a7bbb
Patrick Ofreneo <Patrick Ofreneo@PATRICK-OFRENEO>
2017-11-21 23:23:37 +0600

    Add project files.

---
+++ PreFinalQuiz1_OfreneoL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PreFinalQuiz1_OfreneoL.MvcApplication" Language="C#" %>
---
+++ PreFinalQuiz1_OfreneoL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PreFinalQuiz1_OfreneoL.Startup))]
+namespace PreFinalQuiz1_OfreneoL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
